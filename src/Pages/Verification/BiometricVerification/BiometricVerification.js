cde3219540b337683fda3114df6d4b3061c5f8b3
Asif Khan <asifkhan@Asifs-MacBook-Pro.local>
2023-09-14 12:58:36 +0600

    face verification is complete

--- src/Pages/Verification/BiometricVerification/BiometricVerification.js
+++ src/Pages/Verification/BiometricVerification/BiometricVerification.js
@@ -1,44 +1,95 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import biomatric from'../../../assets/feature-biometric-verification.jpg'
 import Footer from '../../Shared/Footer/Footer';
 import { GrNext } from 'react-icons/gr';
-import Clarifai from "clarifai";
+
+import axios from 'axios';
+import Webcam from 'react-webcam';
+
+
 const BiometricVerification = () => {
+  const webcamRef = useRef(null);
+ 
+  const [apiKey, setApiKey] = useState('KTZE0ID-UNJBBQPm6qLTVgOU4nd6p5pC');
+  const [apiSecret, setApiSecret] = useState('bXYBa-2BNp3TAvZ-MipX2uqdMDviF-Wa');
+  const [faceToken1, setFaceToken1] = useState('');
+  const [imageURL1, setImageURL1] = useState('');
+  const [faceToken2, setFaceToken2] = useState('');
+  const [imageURL2, setImageURL2] = useState('');
+  const [comparisonResult, setComparisonResult] = useState('');
 
+  const imgStorageKey = "058582fd7ce562d09cfe637cf2190600";
+  const handleImageUpload1 = async (event) => {
+    const file = event.target.files[0];
+    const formData = new FormData();
+    formData.append("image", file);
+    const url = `https://api.imgbb.com/1/upload?key=${imgStorageKey}`;
+    fetch(url, {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((result) => setImageURL1(result.data.url))
   
-  const [imageUrl, setImageUrl] = useState("");
-  const [authenticationResult, setAuthenticationResult] = useState("");
+    // console.log(imageURL1);
+  };
 
-  const clarifaiApp = new Clarifai.App({
-    apiKey: "9412f148038b43048d4af6487588246c",
-  });
-  async function authenticateFace(imageUrl) {
-  try {
-    const response = await clarifaiApp.models.predict(
-      Clarifai.FACE_DETECT_MODEL,
-      imageUrl
-    );
+  const captureWebcamScreenshot = async () => {
+    const screenshot = webcamRef.current.getScreenshot();
+    const screenshotBlob = dataURItoBlob(screenshot);
+console.log(screenshotBlob);
+    const formData = new FormData();
+    formData.append("image", screenshotBlob);
+    const url = `https://api.imgbb.com/1/upload?key=${imgStorageKey}`;
+    fetch(url, {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((result) => setImageURL2(result.data.url))
+  
 
-    // Check the response for face detection
-    if (response.outputs[0].data.regions.length > 0) {
-      // Face detected
-      console.log('Face detected. Authentication successful.');
-    } else {
-      // No face detected
-      console.log('No face detected. Authentication failed.');
+  };
+  const dataURItoBlob = (dataURI) => {
+    const byteString = atob(dataURI.split(',')[1]);
+    const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+    const ab = new ArrayBuffer(byteString.length);
+    const ia = new Uint8Array(ab);
+    for (let i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
     }
-  } catch (error) {
-    console.error('Error authenticating face:', error);
-  }
-  }
-    const handleImageUpload = async () => {
-      setAuthenticationResult(""); // Clear previous result
-      if (imageUrl) {
-        await authenticateFace(imageUrl);
-      } else {
-        console.log("Please provide an image URL.");
+    return new Blob([ab], { type: mimeString });
+  };
+
+  const compareFaces = async () => {
+    try {
+      const formData ={
+        api_key : apiKey,
+        api_secret : apiSecret,
+        image_url1 : imageURL1,
+        image_url2 : imageURL2
       }
-    };
+     
+      console.log(formData);
+      
+      const response = await axios.post(
+        'https://api-us.faceplusplus.com/facepp/v3/compare',
+        formData,
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        }
+      );
+
+      setComparisonResult(response.data);
+    } catch (error) {
+      console.error('Error comparing faces:', error);
+    }
+  };
+
+
+
   return (
     <div className="bg-white text-black">
       <div className="py-20 bg-[#f4f4ff]">
@@ -104,19 +155,65 @@ const BiometricVerification = () => {
         </div>
       </div>
       <>
-        <div>
-          <h1>Face Authentication</h1>
+      <div className=''>
+      <h1>Face Comparison</h1>
+      <div>
+        <label>
+          API Key:
           <input
             type="text"
-            placeholder="Enter image URL"
-            value={imageUrl}
-            onChange={(e) => setImageUrl(e.target.value)}
+            value={apiKey}
+            onChange={(e) => setApiKey(e.target.value)}
           />
-          <button onClick={handleImageUpload}>Authenticate</button>
-          <div>
-            <p>Authentication Result: {authenticationResult}</p>
-          </div>
+        </label>
+      </div>
+      <div>
+        <label>
+          API Secret:
+          <input
+            type="text"
+            value={apiSecret}
+            onChange={(e) => setApiSecret(e.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <h2>Face 1</h2>
+        <Webcam className='mx-auto items-center'
+        audio={false}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+      />
+      <button className='btn rounded-full mb-10' onClick={captureWebcamScreenshot}>Capture Webcam Photo</button>
+      {imageURL2 && (
+        <div>
+          <h2>Webcam Photo</h2>
+          <img className='mx-auto items-center' src={imageURL2} alt="Webcam" />
         </div>
+      )}
+
+        
+        <input
+          type="file"
+          accept="image/*"
+          onChange={(e) => handleImageUpload1(e)}
+        />
+        {
+          imageURL1 && (
+            <img className='mx-auto items-center' src={imageURL1} alt="Image 1" />
+          )
+        }
+       
+      </div>
+
+      <button className='btn' onClick={compareFaces}>Compare Faces</button>
+      {comparisonResult && (
+        <div>
+          <h2>Comparison Result</h2>
+          <pre>{JSON.stringify(comparisonResult, null, 2)}</pre>
+        </div>
+      )}
+    </div>
       </>
       <Footer />
     </div>
